0bedb2c87201e036024b7b4d5c414fff9b4671a2
Taewan Kang <tkang042@uottawa.ca>
2020-08-08 22:41:04 -0400

    edited service svg with illustrator to match size with others and revert back trinityitem

--- client/src/components/TrinityItem.js
+++ client/src/components/TrinityItem.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import "./TrinityItem.scss";
 
-export const TrinityItem = ({ img_url, title, href, onClick, className }) => (
+export const TrinityItem = ({ img_url, title, href, onClick }) => (
   <a href={href} className="TrinityItem fcol-md-4" onClick={onClick}>
     <div className="TrinityItem__Title">{title}</div>
-    <div className={className}>
+    <div className="TrinityItem__Img">
       <img aria-hidden="true" src={img_url} />
     </div>
   </a>
